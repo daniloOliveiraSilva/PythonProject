f1aec84fb5de2e587043d96b1bf65e067eb8585f
daniloOliveiraSilva <danilo.oliveira.a.silva@gmail.com>
2024-11-09 12:14:45 -0300

    Incluindo arquivo Logica_python

---
+++ Logica_python.py
@@ -0,0 +1,57 @@
+from operator import truediv
+
+def calcula (num1,num2):
+     resultado = num1 + num2
+     return resultado
+
+valor = calcula(2, 5)
+
+print (valor)
+
+
+def frase ():
+    print ('curso RPA')
+
+frase()
+
+#funcionarios = ['Danilo', 'Pamella' , 'Pedro']
+
+#for item in funcionarios:
+   # print(item)
+
+#numero=10
+#texto= "Charles"
+#verdade= True5
+'''
+while numero <= 20:
+    print (numero)
+    numero = numero + 1
+
+#print('numero: ' + str(numero), 'texto: '+texto, 'verdade:  ' + str(verdade))
+'''
+'''''
+if texto == 'Charles':
+    print(texto)
+    print(numero)
+else:
+    texto= "Danilo"
+    numero= 37
+    print(texto)
+    print(numero)
+
+    # IF TESTANDO DIFERENTE
+
+if texto != 'Charles':
+    print(texto)
+    print(numero)
+else:
+    texto= "Danilo"
+    numero= 37
+    print(texto)
+    print(numero)
+'''
+    #LISTA E LOOP
+
+
+
+
